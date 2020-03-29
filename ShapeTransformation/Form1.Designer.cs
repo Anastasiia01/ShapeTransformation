4364628b29c8c6c7fc7151f2ff22f026f6d591f3
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2020-03-28 20:26:28 -0400

    first commit. I created a form with panels. Initialized shape1 and draw it on one of the panels of our form.

---
+++ ShapeTransformation/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ShapeTransformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panInitial = new System.Windows.Forms.Panel();
+            this.panTransformed = new System.Windows.Forms.Panel();
+            this.btnInitializeShapes = new System.Windows.Forms.Button();
+            this.btnApplyTransformation = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panInitial
+            // 
+            this.panInitial.Location = new System.Drawing.Point(57, 29);
+            this.panInitial.Name = "panInitial";
+            this.panInitial.Size = new System.Drawing.Size(371, 511);
+            this.panInitial.TabIndex = 0;
+            // 
+            // panTransformed
+            // 
+            this.panTransformed.Location = new System.Drawing.Point(521, 29);
+            this.panTransformed.Name = "panTransformed";
+            this.panTransformed.Size = new System.Drawing.Size(371, 511);
+            this.panTransformed.TabIndex = 1;
+            // 
+            // btnInitializeShapes
+            // 
+            this.btnInitializeShapes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnInitializeShapes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInitializeShapes.Location = new System.Drawing.Point(119, 568);
+            this.btnInitializeShapes.Name = "btnInitializeShapes";
+            this.btnInitializeShapes.Size = new System.Drawing.Size(234, 43);
+            this.btnInitializeShapes.TabIndex = 2;
+            this.btnInitializeShapes.Text = "Initialize Shapes";
+            this.btnInitializeShapes.UseVisualStyleBackColor = false;
+            this.btnInitializeShapes.Click += new System.EventHandler(this.btnInitializeShapes_Click);
+            // 
+            // btnApplyTransformation
+            // 
+            this.btnApplyTransformation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnApplyTransformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApplyTransformation.Location = new System.Drawing.Point(594, 568);
+            this.btnApplyTransformation.Name = "btnApplyTransformation";
+            this.btnApplyTransformation.Size = new System.Drawing.Size(242, 43);
+            this.btnApplyTransformation.TabIndex = 3;
+            this.btnApplyTransformation.Text = "Apply Transformation";
+            this.btnApplyTransformation.UseVisualStyleBackColor = false;
+            this.btnApplyTransformation.Click += new System.EventHandler(this.btnApplyTransformation_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 635);
+            this.Controls.Add(this.btnApplyTransformation);
+            this.Controls.Add(this.btnInitializeShapes);
+            this.Controls.Add(this.panTransformed);
+            this.Controls.Add(this.panInitial);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panInitial;
+        private System.Windows.Forms.Panel panTransformed;
+        private System.Windows.Forms.Button btnInitializeShapes;
+        private System.Windows.Forms.Button btnApplyTransformation;
+    }
+}
+
---
+++ ShapeTransformation/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShapeTransformation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Point> Shape1 = new List<Point>();
+        List<Point> Shape2 = new List<Point>();
+        private void btnInitializeShapes_Click(object sender, EventArgs e)
+        {
+            Shape1.Clear(); 
+            Shape2.Clear();
+            Point p1a = new Point(20, 30);
+            Point p2a = new Point(120, 50);
+            Point p3a = new Point(160, 80); 
+            Point p4a = new Point(180, 300);
+            Point p5a = new Point(100, 220);
+            Point p6a = new Point(50, 280); 
+            Point p7a = new Point(20, 140);
+            Shape1.Add(p1a); Shape1.Add(p2a); 
+            Shape1.Add(p3a); Shape1.Add(p4a); 
+            Shape1.Add(p5a); Shape1.Add(p6a);
+            Shape1.Add(p7a);
+            /*Transformation T = new Transformation();
+            T.A = 1.05; T.B = 0.05; T.T1 = 15; T.T2 = 22;
+            Shape2 = ApplyTransformation(T, Shape1); 
+            Shape2[2] = new Point(Shape2[2].X + 10, Shape2[2].Y + 3);*/// change one point-add outlier
+            Pen pBlue = new Pen(Brushes.Blue, 1);
+            Pen pRed = new Pen(Brushes.Red, 1);
+            Graphics g = panInitial.CreateGraphics();
+            DisplayShape(Shape1, pBlue, g);
+            //DisplayShape(Shape2, pRed, g);
+        }
+
+        void DisplayShape(List<Point> Shp, Pen pen, Graphics g)
+        {
+            Point? prev = null;
+            foreach(Point point in Shp)
+            {
+                g.DrawEllipse(pen, new Rectangle(point.X - 2, point.Y-2, 4, 4));
+                if (prev != null)
+                {
+                    g.DrawLine(pen, point, (Point)prev);
+                }
+                prev = point;                
+            }
+            g.DrawLine(pen, Shp[0], Shp[Shp.Count - 1]);
+            
+        }
+
+        private void btnApplyTransformation_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
